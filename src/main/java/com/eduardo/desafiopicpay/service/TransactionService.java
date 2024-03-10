d0f3cd9d66bc8e87f1d23650456853d59fa6d0c6
Eduardo Dantas <eduardodanttas@icloud.com>
2024-03-10 12:13:32 -0300

    feat: create TransactionService

---
+++ src/main/java/com/eduardo/desafiopicpay/service/TransactionService.java
@@ -0,0 +1,59 @@
+package com.eduardo.desafiopicpay.service;
+
+import com.eduardo.desafiopicpay.domain.entities.Transaction;
+import com.eduardo.desafiopicpay.domain.entities.User;
+import com.eduardo.desafiopicpay.dto.TransactionDTO;
+import com.eduardo.desafiopicpay.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+public class TransactionService {
+    @Autowired
+    private TransactionRepository repository;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void createTransaction(TransactionDTO transaction) throws Exception {
+        User sender = userService.findById(transaction.senderId());
+        User receiver = userService.findById(transaction.receiverId());
+
+        userService.validateTransaction(transaction.value(), sender);
+
+        boolean isAuthorized = authorizeTransaction(transaction.value(), sender);
+        if (!isAuthorized){
+            throw new Exception("Invalid transaction");
+        }
+
+        Transaction newTransaction = new Transaction();
+        newTransaction.setValue(transaction.value());
+        newTransaction.setReceiver(receiver);
+        newTransaction.setSender(sender);
+        newTransaction.setTransactionDate(LocalDateTime.now());
+
+        sender.setBalance(sender.getBalance().subtract(transaction.value()));
+        receiver.setBalance(receiver.getBalance().add(transaction.value()));
+        repository.save(newTransaction);
+        userService.saveUser(sender);
+        userService.saveUser(receiver);
+    }
+
+    public boolean authorizeTransaction(BigDecimal value, User sender){
+        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
+
+        if (authorizationResponse.getStatusCode() == HttpStatus.OK){
+            String message = (String) authorizationResponse.getBody().get("message");
+            return "Autorizado".equalsIgnoreCase(message);
+        } else return false;
+    }
+}
