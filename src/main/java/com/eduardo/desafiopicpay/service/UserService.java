42b993c8cea1ca112825536f648bc48e5ee05385
Eduardo Dantas <eduardodanttas@icloud.com>
2024-03-10 18:43:56 -0300

    fix: Adjust Exception message

--- src/main/java/com/eduardo/desafiopicpay/service/UserService.java
+++ src/main/java/com/eduardo/desafiopicpay/service/UserService.java
@@ -4,6 +4,7 @@ import com.eduardo.desafiopicpay.domain.entities.User;
 import com.eduardo.desafiopicpay.domain.enums.UserType;
 import com.eduardo.desafiopicpay.dto.UserDTO;
 import com.eduardo.desafiopicpay.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -18,7 +19,7 @@ public class UserService {
 
     public void validateTransaction(BigDecimal value, User sender) throws Exception {
         if (sender.getUserType() == UserType.MERCHANT){
-            throw new Exception("User can't send money");
+            throw new Exception("User with MERCHANT type can't send money");
         }
         if (sender.getBalance().compareTo(value) < 0){
             throw new Exception("User don't have money enough to make a transaction");
@@ -26,7 +27,7 @@ public class UserService {
     }
 
     public User findById(Long id) throws Exception {
-        return repository.findById(id).orElseThrow(Exception::new);
+        return repository.findById(id).orElseThrow(() -> new EntityNotFoundException("User can't be found in database"));
     }
 
     public User createUser(UserDTO userDTO){
