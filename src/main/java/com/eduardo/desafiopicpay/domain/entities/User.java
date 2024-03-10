b61d03fd1f03e21b6a0930cff7f49bcc7614a693
Eduardo Dantas <eduardodanttas@icloud.com>
2024-03-10 10:57:34 -0300

    feat: create User entity

---
+++ src/main/java/com/eduardo/desafiopicpay/domain/entities/User.java
@@ -0,0 +1,30 @@
+package com.eduardo.desafiopicpay.domain.entities;
+
+import com.eduardo.desafiopicpay.domain.enums.UserType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "tb_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String document;
+    private BigDecimal balance;
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+}
---
+++ src/main/java/com/eduardo/desafiopicpay/domain/enums/UserType.java
@@ -0,0 +1,6 @@
+package com.eduardo.desafiopicpay.domain.enums;
+
+public enum UserType {
+    COMMON,
+    MERCHANT;
+}
